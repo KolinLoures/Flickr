5cd5a5e29a43563ae911760f52f22437deb988e4
n.kirilov <kolin-1994@mail/ru>
2016-05-24 16:31:58 +0300

    add IntentService/AlarmReceiver

--- app/src/main/java/com/example/kolin/flick/PhotoGalleryActivity.java
+++ app/src/main/java/com/example/kolin/flick/PhotoGalleryActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class PhotoGalleryActivity extends AppCompatActivity {
 
@@ -12,6 +13,9 @@ public class PhotoGalleryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.container);
